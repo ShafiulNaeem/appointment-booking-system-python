77cf824286c1007c0ca4826695c087701dac97df
ShafiulNaeem <shafiulnaeemrifat@gmail.com>
2025-04-29 22:51:45 +0600

    add user model

--- app/models/User.py
+++ app/models/User.py
@@ -1,2 +1,8 @@
-class User:
+from app.utils.lib.Model import Model
+
+class User(Model):
+    def __init__(self):
+        super().__init__()
+        self.__table = "users"
+        self.__primary_key = "id"
     
